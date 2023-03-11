7c6ecac43b6ead8b8299311db396e2eda71bdd57
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-03-10 21:37:38 -0300

    refactoring all code after a webpack error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Header() {
   return (
     <header className={styles.postHeader}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
@@ -24,89 +25,52 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home({ postsPagination }: HomeProps) {
   return (
     <>
-      <main>
+      <main className={commonStyles.container}>
         <Header />
       </main>
       <section className={styles.container}>
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <span>
-              <FiCalendar /> 09 Mar 23
-            </span>
-            <span>
-              <FiUser /> Joseph Oliveira
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1>Criando um app CRA do zero</h1>
-          <p>
-            Tudo sobre como criar a sua primeira aplicação utilizando Create
-            React App
-          </p>
-          <div>
-            <span>
-              <FiCalendar /> 19 Abr 23
-            </span>
-            <span>
-              <FiUser /> Danilo Vieira
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <span>
-              <FiCalendar /> 09 Mar 23
-            </span>
-            <span>
-              <FiUser /> Joseph Oliveira
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <span>
-              <FiCalendar /> 09 Mar 23
-            </span>
-            <span>
-              <FiUser /> Joseph Oliveira
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.post}>
-          <h1>Como utilizar Hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div>
-            <span>
-              <FiCalendar /> 09 Mar 23
-            </span>
-            <span>
-              <FiUser /> Joseph Oliveira
-            </span>
-          </div>
-        </div>
+        {postsPagination.results.map(post => (
+          <a className={styles.post}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <span>
+                <FiCalendar /> {post.first_publication_date}
+              </span>
+              <span>
+                <FiUser /> {post.data.author}
+              </span>
+            </div>
+          </a>
+        ))}
         <span className={styles.morePosts}>Carregar mais posts</span>
       </section>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('Posts', {
+    pageSize: 3,
+    orderings: {
+      field: 'last_publication_date',
+      direction: 'desc',
+    },
+  });
 
-//   // TODO
-// };
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
